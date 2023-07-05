81148b564090fa3375b3de75b5bbae32f06348cf
Daniel Forero <daniel.w.forero.ctr@army.mil>
2023-07-05 16:46:13 -0500

    Simple server setup

--- Section 2: Intro to Node and NPM/Module 1: Core Modules/index.js
+++ Section 2: Intro to Node and NPM/Module 1: Core Modules/index.js
@@ -1,4 +1,9 @@
 const fs = require('fs')
+const http = require('http')
+
+/*
+Read and Write to File System
+ */
 
 // Used to read and write files from and to the file system SYNCHRONOUSLY (BLOCKING)
 // const textInput = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -10,14 +15,26 @@ const fs = require('fs')
 
 // Used to read and write files from and to the file system ASYNCHRONOUSLY (NON-BLOCKING)
 // This uses callback hell, and the better method will be discussed later
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-  if(err) return console.error('Error!')
-  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-    fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
-      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-        console.log('Your file has been written!')
-      })
-    })
-  })
-});
-console.log('Reading file...')
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   if(err) return console.error('Error!')
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//         console.log('Your file has been written!')
+//       })
+//     })
+//   })
+// });
+// console.log('Reading file...')
+
+/******************
+SERVER
+ *******************/
+
+const server = http.createServer((req, res) => {
+  res.end('Hello from the server!');
+})
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000...')
+})
