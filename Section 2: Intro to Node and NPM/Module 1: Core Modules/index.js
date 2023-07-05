7f2e9f196913b36767276b15555011cf78451cb4
Daniel Forero <daniel.w.forero.ctr@army.mil>
2023-07-05 13:12:35 -0500

    Blocking vs Non-Blocking

--- Section 2: Intro to Node and NPM/Module 1: Core Modules/index.js
+++ Section 2: Intro to Node and NPM/Module 1: Core Modules/index.js
@@ -1,12 +1,16 @@
 const fs = require('fs')
 
-// Used to read files from the file system
-const textInput = fs.readFileSync('./input.txt', 'utf-8');
-console.log(textInput)
+// Used to read and write files from and to the file system SYNCHRONOUSLY
+// const textInput = fs.readFileSync('./input.txt', 'utf-8');
+// console.log(textInput)
+//
+// const textOutput = `This is what we know about the avocado: ${textInput}`;
+// fs.writeFileSync('./output.txt', textOutput);
+// console.log('File written')
 
-// Used to write files to the file system
-const textOutput = `This is what we know about the avocado: ${textInput}`;
-fs.writeFileSync('./output.txt', textOutput);
-
-console.log('File written')
+// Used to read and write files from and to the file system ASYNCHRONOUSLY
+fs.readFile('./input.txt', 'utf-8', (err, data) => {
+  console.log(data);
+});
+console.log('Reading file...')
 
