ab639a3602549d5c2fcfb44efbf5aa49e390b283
Daniel Forero <dforero@vmware.com>
2023-07-05 17:05:05 -0500

    Simple routing technique and response headers

--- Section 2: Intro to Node and NPM/Module 1: Core Modules/index.js
+++ Section 2: Intro to Node and NPM/Module 1: Core Modules/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const http = require('http')
+const url = require('url')
 
 /*
 Read and Write to File System
@@ -32,9 +33,22 @@ SERVER
  *******************/
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from the server!');
-})
+  const pathname = req.url;
+
+  if(pathname === '/' || pathname === '/overview'){
+    res.end('This is the OVERVIEW!')
+  }else if(pathname === '/product'){
+    res.end('This is the PRODUCT')
+  }else{
+    // Header information should always be set before the res.end
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world'
+    })
+    res.end('<h1>This page cannot be found.</h1>')
+  }
+});
 
 server.listen(8000, '127.0.0.1', () => {
-  console.log('Listening to requests on port 8000...')
-})
+  console.log('Listening to requests on port 8000...');
+});
