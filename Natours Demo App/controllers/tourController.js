ee75a33a9656df4c7fec819e88d296462afbcb56
Daniel Forero <dforero@vmware.com>
2023-07-11 12:19:48 -0500

    Param middleware

--- Natours Demo App/controllers/tourController.js
+++ Natours Demo App/controllers/tourController.js
@@ -4,6 +4,15 @@ const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkId = (req, res, next, val) => {
+  console.log(`Tour id is: ${val}`)
+  if (val > tours.length - 1) return res.status(404).json({
+    status: 'fail',
+    message: 'Invalid tour ID!'
+  });
+  next();
+}
+
 ////////////////////////////////////////////
 // Route Handlers
 exports.getAllTours = (req, res) => {
@@ -13,22 +22,6 @@ exports.getAllTours = (req, res) => {
     results: tours.length,
     data: {
       tours
-    }
-  })
-}
-exports.getTourById = (req, res) => {
-  const id = Number(req.params.id);
-  const tour = tours.find(el => el.id === id)
-
-  if (!tour) return res.status(404).json({
-    status: 'fail',
-    message: 'Invalid tour ID!'
-  })
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour
     }
   })
 }
@@ -44,13 +37,18 @@ exports.createTour = (req, res) => {
     })
   })
 }
-exports.updateTour = (req, res) => {
+exports.getTourById = (req, res) => {
   const id = Number(req.params.id);
-  if (id > tours.length - 1) return res.status(404).json({
-    status: 'fail',
-    message: 'Invalid tour ID!'
-  })
+  const tour = tours.find(el => el.id === id)
 
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour
+    }
+  })
+}
+exports.updateTour = (req, res) => {
   res.status(200).json({
     status: 'success',
     data: {
@@ -59,12 +57,6 @@ exports.updateTour = (req, res) => {
   })
 }
 exports.deleteTour = (req, res) => {
-  const id = Number(req.params.id);
-  if (id > tours.length - 1) return res.status(404).json({
-    status: 'fail',
-    message: 'Invalid tour ID!'
-  })
-
   res.status(204).json({
     status: 'success',
     data: null
--- Natours Demo App/routes/tourRoutes.js
+++ Natours Demo App/routes/tourRoutes.js
@@ -1,20 +1,15 @@
 const express = require('express');
 const {
-  getAllTours, createTour, getTourById, updateTour, deleteTour
+  getAllTours, createTour, getTourById, updateTour, deleteTour, checkId
 } = require('./../controllers/tourController')
-
-//////////////////////////////////////////
-// Routes
-// Express allows you to just use the HTTP method as a function call
-// app.get('/api/v1/tours', getAllTours);
-// app.get('/api/v1/tours/:id', getTourById);
-// app.post('/api/v1/tours', createTour);
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
 
 // These routes are also part of the middleware stack
 // This is called mounting a router. It creates a router for a specific subdomain
 const router = express.Router();
+
+// This middleware function will only run on this router
+router.param('id', checkId);
+
 // You can also chain HTTP methods from the route method if they all use the same route
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTourById).patch(updateTour).delete(deleteTour);
