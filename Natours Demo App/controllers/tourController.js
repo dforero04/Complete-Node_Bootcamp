6bd18fa2e80ed9da7a2aaef5e3e92f58aa1dd6de
Daniel Forero <dforero@vmware.com>
2023-07-13 14:11:38 -0500

    Document, Query, and Aggregate middleware, virtuals

--- Natours Demo App/controllers/tourController.js
+++ Natours Demo App/controllers/tourController.js
@@ -57,30 +57,27 @@ exports.getTourById = async (req, res) => {
 
 exports.getTourStats = async (req, res) => {
   try {
-    const stats = await Tour.aggregate(
-      [
-        { $match: { ratingsAverage: { $gte: 4.5 } } },
-        {
-          $group: {
-            _id: { $toUpper: '$difficulty' },
-            numTours: { $sum: 1 },
-            numRatings: { $sum: '$ratingsQuantity' },
-            avgRating: { $avg: '$ratingsAverage' },
-            avgPrice: { $avg: '$price' },
-            minPrice: { $min: '$price' },
-            maxPrice: { $max: '$price' }
-          }
-        },
-        {
-          $sort: { avgPrice: 1 }
+    const stats = await Tour.aggregate([
+      { $match: { ratingsAverage: { $gte: 4.5 } } },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          numTours: { $sum: 1 },
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' }
         }
-        // You can continue to match to narrow your results
-        // {
-        //   $match: { _id: { $ne: 'EASY' } }
-        // }
-      ],
-      () => {}
-    );
+      },
+      {
+        $sort: { avgPrice: 1 }
+      }
+      // You can continue to match to narrow your results
+      // {
+      //   $match: { _id: { $ne: 'EASY' } }
+      // }
+    ]).exec();
 
     res.status(200).json({
       status: 'success',
@@ -98,45 +95,42 @@ exports.getMonthlyPlan = async (req, res) => {
   try {
     const year = Number(req.params.year);
 
-    const plan = await Tour.aggregate(
-      [
-        {
-          $unwind: '$startDates'
-        },
-        {
-          $match: {
-            startDates: {
-              $gte: new Date(`${year}-01-01`),
-              $lte: new Date(`${year}-12-31`)
-            }
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates'
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`)
           }
-        },
-        {
-          $group: {
-            _id: { $month: '$startDates' },
-            numTourStarts: { $sum: 1 },
-            tours: { $push: '$name' }
-          }
-        },
-        {
-          $addFields: { month: '$_id' }
-        },
-        {
-          $project: {
-            _id: 0
-          }
-        },
-        {
-          $sort: {
-            numTourStarts: -1
-          }
-        },
-        {
-          $limit: 6
         }
-      ],
-      () => {}
-    );
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: '$name' }
+        }
+      },
+      {
+        $addFields: { month: '$_id' }
+      },
+      {
+        $project: {
+          _id: 0
+        }
+      },
+      {
+        $sort: {
+          numTourStarts: -1
+        }
+      },
+      {
+        $limit: 6
+      }
+    ]).exec();
 
     res.status(200).json({
       status: 'success',
--- Natours Demo App/model/tourModel.js
+++ Natours Demo App/model/tourModel.js
@@ -1,57 +1,117 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name!'],
-    unique: true,
-    trim: true
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name!'],
+      unique: true,
+      trim: true
+    },
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration!']
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size!']
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty']
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price!']
+    },
+    discountPrice: Number,
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a summary!']
+    },
+    description: {
+      type: String,
+      trim: true
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image!']
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false // Do not show in query results
+    },
+    startDates: [Date],
+    slug: String,
+    secretTour: {
+      type: Boolean,
+      default: false
+    }
   },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration!']
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a group size!']
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have a difficulty']
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price!']
-  },
-  discountPrice: Number,
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, 'A tour must have a summary!']
-  },
-  description: {
-    type: String,
-    trim: true
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have a cover image!']
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false // Do not show in query results
-  },
-  startDates: [Date]
+  {
+    // Used to show virtual properties in the results
+    toJSON: { virtuals: true }
+  }
+);
+
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
+});
+
+// Document Middleware
+// Runs before provided mongoDB method
+// The THIS keyword points to the current document
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.pre('save', (next) => {
+//   console.log('will save document');
+//   next();
+// });
+//
+// // Runs after provided mongoDB method
+// tourSchema.post('save', (doc, next) => {
+//   console.log(doc);
+//   next();
+// });
+
+// Query Middleware
+// Runs before provided mongoDB query method
+// This REGEX makes it run for all find methods
+// The THIS keyword points to the current query
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took: ${Date.now() - this.start} milliseconds`);
+  next();
+});
+
+// Aggregate Middleware
+// Runs before provided mongoDB aggregate method
+// The THIS keyword points to the current aggregate
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  console.log(this.pipeline());
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
